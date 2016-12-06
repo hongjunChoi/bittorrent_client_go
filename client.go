ce519bdb417bdcd897ebd70d3d008dce4c30441e
hconsg1 <hconsg1>
2016-12-05 23:00:19 -0500

    bencode parser

--- client.go
+++ client.go
@@ -2,29 +2,12 @@ package main
 
 import (
 	"fmt"
-	"./bencode-go"
-	"os"
 )
 
 func main() {
-	fmt.Println("----")
-	file, er := os.Open("trial.torrent")
-	if er != nil {
-		// return false
-		fmt.Println(er)
-	}
-	defer file.Close()
 
-	// Decode bencoded metainfo file.
-	fileMetaData, er := bencode.Decode(file)
-	if er != nil {
-		fmt.Println(er)
-	}
-	metaInfoMap, _ := fileMetaData.(map[string]interface{})
-
-	for k, _ := range metaInfoMap { 
-    	fmt.Printf("key[%s] ", k)
-	}
-
-	fmt.Println(metaInfoMap["url-list"])
+	metaInfo := new(MetaInfo)
+	metaInfo.ReadTorrentMetaInfoFile("trial.torrent")
+	fmt.Println(metaInfo.Info.Files)
+	return
 }
