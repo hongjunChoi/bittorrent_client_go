f3f50255e80002216717f880cdc07ff5cdfbd8c5
Hongjun Choi <hong_jun_choi@brown.edu>
2016-12-06 17:06:37 -0500

    sync

--- client.go
+++ client.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	bencode "./bencode-go"
+	// bencode "./bencode-go"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	metaInfo := new(MetaInfo)
-	metaInfo.ReadTorrentMetaInfoFile("data/trial.torrent")
+	metaInfo.ReadTorrentMetaInfoFile("data/hamlet.torrent")
 	trackerUrl := metaInfo.Announce
 
 	data := parseMetaInfo(metaInfo)
@@ -25,13 +25,18 @@ func main() {
 
 //TODO: COMPLETE THIS PART
 func parseMetaInfo(info *MetaInfo) map[string]string {
+	fmt.Println("===== info hash =====")
+	fmt.Println(info.InfoHash)
+	fmt.Println(url.QueryEscape(info.InfoHash))
+	fmt.Println("=======")
+
 	data := make(map[string]string)
 	data["info_hash"] = url.QueryEscape(info.InfoHash)
 	data["port"] = "6881"
 	data["uploaded"] = "0"
 	data["downloaded"] = "0"
 	data["left"] = "0"
-	data["compact"] = "0"
+	// data["compact"] = "0"
 	// data["no_peer_id"]
 	// data["event"]
 
@@ -58,12 +63,16 @@ func get_peer_list(trackerUrl string, data map[string]string) []string {
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
+
+	// TODO: parse body with bencode
 	// responseData = bencode
+
 	fmt.Println("\n\n======= body =======\n")
-	fmt.Println(body)
+	fmt.Println(string(body))
 	fmt.Println("\n==============")
 
-	return resposneData
+	// return resposneData
+	return make([]string, 0)
 }
 
 func createTrackerQuery(baseUrl string, data map[string]string) string {
