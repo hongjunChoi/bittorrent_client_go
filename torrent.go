5256113f791d58d040fa8b135e6b0c8f7bcc1a12
hconsg1 <hconsg1>
2016-12-06 20:08:31 -0500

    Ysnc

--- torrent.go
+++ torrent.go
@@ -1,5 +1,18 @@
 package main
 
+import(
+	"fmt"
+)
 func (t *Torrent) initBitMap(u int64) {
-	// t.BitMap = 0
+	length := int(u/8)
+	if (u%8 > 0){
+		length += 1
+	} 
+	t.BitMap = make([]byte, length)
+	fmt.Println(t.BitMap[0])
+}
+
+func setBit(n int, pos uint) int {
+    n |= (1 << pos)
+    return n
 }
