883bd1cfda12cf8693c40beea064eb2cf9125d64
hconsg1 <hconsg1>
2016-12-13 19:42:18 -0500

    Sync

--- client.go
+++ client.go
@@ -606,10 +606,9 @@ func (torrent *Torrent) get_peer_list(trackerUrl string, data map[string]string)
 	fmt.Println(peers)
 	fmt.Println("==========")
 
-	peerData := make([]*Peer, len(peers)/6)
+	peerData := make([]*Peer, 0)
 
 	myIP := get_external_IP()
-	count := 0
 	for i := 0; i < len(peers)/6; i++ {
 		index := i * 6
 		ip := peers[index : index+4]
@@ -630,8 +629,7 @@ func (torrent *Torrent) get_peer_list(trackerUrl string, data map[string]string)
 		peer.BlockQueue = lane.NewQueue()
 		peer.PeerQueueMap = make(map[string](*Block))
 		peer.CurrentBlock = 0
-		peerData[count] = peer
-		count += 1
+		peerData = append(peerData, peer)
 	}
 
 	return peerData
